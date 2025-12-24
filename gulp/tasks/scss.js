84b4f597474444835076fffbe1700993d53263ff
Alexandr Petrovich <apalion97@gmail.com>
2025-12-24 19:13:02 +0200

    Remove unused files, update dependencies, and add missing SVG assets
    Unused JavaScript code and configuration files were removed (e.g., `.browserslistrc` and `TestSlider.js`). Updated `package.json` with additional `gulp-purgecss` dependency and redefined `browserslist`. Added multiple missing SVG assets and fixed minor styles/formatting inconsistencies in HTML files.

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -9,6 +9,7 @@ import autoprefixer from 'autoprefixer';
 import postcss from 'gulp-postcss';
 import postcssPresetEnv from 'postcss-preset-env';
 import sourcemaps from "gulp-sourcemaps";
+import purgecss from 'gulp-purgecss';
 
 import { filePaths } from '../config/paths.js';
 import { plugins } from '../config/plugins.js';
@@ -34,6 +35,36 @@ const scss = (isBuild) => {
 
 		.pipe(plugins.if(isBuild, webpCss(webpConfig)))
 		.pipe(plugins.if(isBuild, postcss([autoprefixer(), postcssPresetEnv()])))
+
+		/** Удаление неиспользуемого CSS только для production */
+		.pipe(
+			plugins.if(
+				isBuild,
+				purgecss({
+					content: [
+						'./src/**/*.html',
+						'./src/js/**/*.js',
+					],
+					safelist: {
+						standard: [
+							/^(container(-fluid)?|row|col-(sm|md|lg|xl)?-\d+|col|offset-\d+)$/, // Bootstrap grid
+							/^(show|collapse|collapsing|modal|fade|active|open)$/,
+							/^(dropdown|dropdown-menu|dropdown-item|show)$/,
+							/^swiper(-|__|\b).*/, // Swiper classes
+							/^splide(.*)/, // Splide classes
+							/^aos(-|\b).*/, // AOS classes
+							/^webp$|^no-webp$/,
+							/^is-.*$/,
+							/^js-.*$/,
+						],
+						deep: [
+							/::placeholder/,
+							/::selection/,
+						],
+					},
+				})
+			)
+		)
 
 		/** Раскомментировать если нужен не сжатый дубль файла стилей */
 		// .pipe(gulp.dest(filePaths.build.css))
